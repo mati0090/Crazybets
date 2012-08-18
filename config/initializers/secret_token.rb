7e21e1aceefd47694d4cad0c2ad1ba08664b22ec
Bury <mati0090@gmail.com>
2012-08-18 22:22:31 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crazybets::Application.config.secret_token = '83cf93b61a2ed899c5d9a38e1e4145959afca09995ceb45bfd60c97ca151785e1ceef0612e01d5b79c384b2722de3800ba5ee2a0298c99087ffbe70272039531'
